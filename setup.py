4155e022402dcf9be0b170e9a6b8fb7a895b2595
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2022-10-17 21:22:22 +0800

    alvistack/1.5.0
        git clean -xdf
        tar zcvf ../python-antsibull-core_1.5.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-antsibull-core.spec ../python-antsibull-core_1.5.0-1.spec
        mv ../python*-antsibull-core*1.5.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/ansible-community-antsibull-core-1.5.0/
        rm -rf ../python*-antsibull-core*1.5.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['antsibull_core',
+ 'antsibull_core.schemas',
+ 'antsibull_core.utils',
+ 'antsibull_core.vendored']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML',
+ 'aiofiles',
+ 'aiohttp>=3.0.0',
+ 'packaging>=20.0',
+ 'perky',
+ 'pydantic',
+ 'semantic_version',
+ 'sh',
+ 'twiggy>=0.5.0']
+
+extras_require = \
+{':python_version < "3.8"': ['importlib-metadata'],
+ ':python_version >= "3.6" and python_version < "3.7"': ['aiocontextvars']}
+
+setup_kwargs = {
+    'name': 'antsibull-core',
+    'version': '1.5.0',
+    'description': 'Tools for building the Ansible Distribution',
+    'author': 'Toshio Kuratomi',
+    'author_email': 'a.badger@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/ansible-community/antsibull-core',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.6.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
